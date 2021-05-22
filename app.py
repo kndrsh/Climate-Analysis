7e06aa89172a772b392d5fc5c3321d800ac9b296
Isha kundra <kndrsh@gmail.com>
2021-05-22 00:52:43 -0700

    added routes

--- app.py
+++ app.py
@@ -9,3 +9,32 @@ import  numpy as np
 
 #Database Setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+#Reflect a database into a new model 
+
+Base = automap_base()
+
+# Reflect the tables 
+
+Base.prepare(engine,reflect=True)
+
+#Save references to each table 
+
+measurement = Base.classes.measurement
+Station = Base.classes.station 
+
+#Setup Flask 
+app = Flask(__name__)
+
+#Routes 
+@app.route("/")
+def welcome():
+    """List all api routes."""
+    return(
+        f"Available Routes:<br/> "
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+    )
